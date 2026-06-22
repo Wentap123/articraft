934d5cbbc820f4068a4ca33a408b34c3f9b39af7
Wentao <wentap@WentaodeMacBook-Air.local>
2026-06-22 21:47:58 +0800

    Import mobility record rec_100174

---
+++ data/records/rec_100174/revisions/rev_000001/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c61de528ecd188b39042862094721b8ff69affbc5add755ad23d3be941bb286e
+size 242
--- scripts/import_mobility_record.py
+++ scripts/import_mobility_record.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+c#!/usr/bin/env python3
 from __future__ import annotations
 
 import argparse
